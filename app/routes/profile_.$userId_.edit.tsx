6ead98dd17af08599204b6bdd3a9db383281e824
Toni <85411437+antonioGoeminne@users.noreply.github.com>
2024-08-06 22:30:37 -0300

    feat: adding get profile data in formEdit form (#21)

--- app/routes/profile_.$userId_.edit.tsx
+++ app/routes/profile_.$userId_.edit.tsx
@@ -7,20 +7,31 @@ import { HeaderBack } from "features/layout/HeaderBack";
 import { Avatar } from "ui/Avatar/Avatar";
 import avatar from "../../images/avatar.png";
 import { FormEditProfile } from "features/profile/components/FormEditProfile";
-import { updateProfile } from "features/profile/services";
+import { getProfile, updateProfile } from "features/profile/services";
 import invariant from "tiny-invariant";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Edit Profile" }];
 };
 
+export async function loader() {
+  const data = await getProfile();
+
+  return {
+    user: data,
+  };
+}
+
 export default function ProfilePage() {
+  const { user } = useLoaderData<typeof loader>();
+
   return (
     <div className="h-full fixed z-20 bg-white w-full py-12 flex flex-col items-center px-4 overflow-y-auto">
       <HeaderBack label="Edit profile" />
       <div className="my-4 flex flex-col justify-center items-center w-full">
         <Avatar size={"md"} src={avatar} alt="profile" />
-        <FormEditProfile />
+        <FormEditProfile user={user} />
       </div>
     </div>
   );
